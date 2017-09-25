0c2298d28b0a634c11ec7b3be870b7e22912f1fa
diftarustiadi <32253995+diftarustiadi@users.noreply.github.com>
2017-09-25 12:10:36 +0700

    LEVEL 2 - Connecting to the Database using PHP

--- login.php
+++ login.php
@@ -3,8 +3,13 @@
  	$username = $_POST['username'];
  	$password = $_POST['password'];
  
- 	echo $username;
- 	echo $password;
+ $koneksi = mysqli_connect("localhost", "root", "", "belajar_loginapp" );
+ 
+ 	if($koneksi){
+ 		echo "We're connected";
+ 	}else{
+ 		echo "Connection failed";
+ 	}
  }
  ?>
  
