0e21ebb87bca08afdf72e0109ed4648da5dd0004
diftarustiadi <32253995+diftarustiadi@users.noreply.github.com>
2017-10-06 00:07:42 +0700

    Dosen-Delete

--- pert2/proses_dsn.php
+++ pert2/proses_dsn.php
@@ -4,7 +4,7 @@
  
  if($_GET['action'] == "add"){
  	//2. Query 
- 	$query = "INSERT INTO mahasiswa (kode_dosen, nama)
+ 	$query = "INSERT INTO dosen (kode_dosen, nama)
  			VALUES('$_POST[kode_dosen]', '$_POST[nama]')";
  }else if($_GET['action'] == 'edit'){
  	//2. Query 
@@ -12,3 +12,12 @@
  				SET kode_dosen = '$_POST[kode_dosen]',
  					nama = '$_POST[nama]',
  				WHERE id = $_POST[id]";
+ }else if($_GET['action'] == "delete"){
+ 	$query = "DELETE FROM dosen 
+ 				WHERE id=$_GET[id]";
+ }
+ 
+ mysqli_query($koneksi, $query);
+ 
+ //Redirect ke template.php
+ header('Location: template.php?page=dosen');
