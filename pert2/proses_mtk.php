0dc14122a99559ceff965dbb1db2527f2739e2ca
diftarustiadi <32253995+diftarustiadi@users.noreply.github.com>
2017-10-06 12:15:35 +0700

    MataKuliah-Update

---
+++ pert2/proses_mtk.php
@@ -0,0 +1,14 @@
+<? //filename: proses_mtk.php 
+ 
+ include("db.php");
+ 
+ if($_GET['action'] == "add"){
+ 	//2. Query 
+ 	$query = "INSERT INTO matakuliah (kode_mtk, nama_mtk)
+ 			VALUES('$_POST[kode_mtk]', '$_POST[nama_mtk]')";
+ }else if($_GET['action'] == 'edit'){
+ 	//2. Query 
+ 	$query = = "UPDATE matakuliah 
+ 				SET kode_dosen = '$_POST[kode_mtk]',
+ 					nama = '$_POST[nama_mtk]',
+ 				WHERE id = $_POST[id]";
