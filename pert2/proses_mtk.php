367a4689404015ffc9e3c38b1c7d769da44d6996
diftarustiadi <32253995+diftarustiadi@users.noreply.github.com>
2017-10-06 12:16:47 +0700

    MataKuliah-Delete

--- pert2/proses_mtk.php
+++ pert2/proses_mtk.php
@@ -12,3 +12,12 @@
  				SET kode_dosen = '$_POST[kode_mtk]',
  					nama = '$_POST[nama_mtk]',
  				WHERE id = $_POST[id]";
+ }else if($_GET['action'] == "delete"){
+ 	$query = "DELETE FROM matakuliah 
+ 				WHERE id=$_GET[id]";
+ }
+ 
+ mysqli_query($koneksi, $query);
+ 
+ //Redirect ke template.php
+ header('Location: template.php?page=matakuliah');
