ed4cd88b3cdd37ec9baff86b9d42a53ae8e62726
diftarustiadi <32253995+diftarustiadi@users.noreply.github.com>
2017-11-09 23:27:01 +0700

    087 - Retrieving data from MySQL

--- pert5/databases.php
+++ pert5/databases.php
@@ -12,9 +12,46 @@
 		);
 	}
 ?>
-
-
 <?php
-	//5. Close database connection
-mysqli_close($connection);
-?>
+ 	// 2. Perform database query
+ 	$query = "SELECT * ";
+ 	$query .= "FROM subjects ";
+ 	$query .= "WHERE visible = 1";
+ 	$query .= "ORDER BY position ASC"
+ 
+ 	$result = mysqli_query($connection, $query);
+ 	if (!$result) {
+ 		die("Database query failed");
+ 	}
+ ?>
+ 
+ <!DOCTYPE html>
+ <html lang="en">
+ 	<head>
+ 		<title>Databases</title>
+ 	</head>
+ 	<body>
+ 		
+ 		<?php
+ 		// 3. Use returned data (if any)
+ 		while($row = mysqli_fetch_row($result)) {
+ 			// output data from each row
+ 			var_dump($row);
+ 			echo "<hr />"
+ 		}
+ 		?>
+ 
+ 		<?php
+ 		// 4. Release returnedd data
+ 		mysqli_free_result($result);
+ 		?>
+ 
+ 	</body>
+ </html>
+ 
+ 
+ <?php
+ 	//5. Close database connection
+ mysqli_close($connection);
+ ?> 
+ 
